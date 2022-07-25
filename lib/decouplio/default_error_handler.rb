1b368782bcd318d2ca3d6501805cbc50e11bde8f
Al Bal <differencialx@gmail.com>
2022-07-26 02:58:21 +0300

    Extend error store functionality

--- lib/decouplio/default_error_handler.rb
+++ lib/decouplio/default_error_handler.rb
@@ -2,17 +2,32 @@
 
 module Decouplio
   class DefaultErrorHandler
-    attr_reader :errors
+    attr_reader :errors, :status
 
     def initialize
       @errors = {}
+      @status = nil
     end
 
-    def add_error(key, message)
-      @errors.store(
-        key,
-        (@errors[key] || []) + [message].flatten
-      )
+    def add_error(*args)
+      case args.size
+      when 1
+        args[0].each do |key, message|
+          @errors.store(
+            key,
+            (@errors[key] || []) + [message].flatten
+          )
+        end
+      when 2
+        @errors.store(
+          args[0],
+          (@errors[args[0]] || []) + [args[1]].flatten
+        )
+      end
+    end
+
+    def error_status(err_st)
+      @status = err_st
     end
 
     def merge(error_store)
